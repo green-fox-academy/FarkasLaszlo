d208105052655ef8bcc25cb550f9635d4b3b9be7
FarkasLaszlo <wolfi1234@gmail.com>
2017-11-27 16:42:42 +0100

    I corrected the cuboid

--- week-02/day-01/Exercise12.py
+++ week-02/day-01/Exercise12.py
@@ -3,8 +3,13 @@
 # 
 # Surface Area: 600
 # Volume: 1000
-a = 2
-surface = 6*a**2
-volume = a**3
+a = input("First side of the cube: ")
+b = input("Second side of the cube: ")
+c = input("Third side of the cube: ")
+a = int(a)
+b = int(b)
+c = int(c)
+surface = a*b*2 + b*c*2 + c*a*2
+volume = a*b*c
 print("Surface Area:",surface)
 print("Volume:",volume)
