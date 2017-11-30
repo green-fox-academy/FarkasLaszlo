c82ff07af975d264e969c497ef3c73fa72bb2f99
Farkas László <wolfi1234@gmail.com>
2017-11-30 20:28:51 +0100

    I created a short and simple version

--- week-02/day-04/04 part of integer.py
+++ week-02/day-04/04 part of integer.py
@@ -1,18 +1,17 @@
-input_list = [ 1, 2, 3, 25, 21, 1, 32, 31, 41, 65, 45167, 161, 710, 18, 18312, 818]
+input_list = [19, 2, 3, 25, 21, 19, 32, 31, 41, 65, 45167, 161, 710, 18, 18312, 818]
 number = 7
 
 def part(input_list, number):
     out = []
     for i in range(len(input_list)):
+        n = 0
         for x in range(len(str(input_list[i]))):
-            if input_list[i] == number:
-                out.append(i)
-            elif input_list[i] % (10*(x+1)) == number:
-                out.append(i)
-            elif x > 0:
-                if int((input_list[i] / (10**x)) % 10) == number:
+            if int((input_list[i] / (10**x)) % 10) == number:
+                if n == 0:
                     out.append(i)
+                    n += 1
 
-    out = sorted(set(out))
+
     return out
+
 print(part(input_list,number))
