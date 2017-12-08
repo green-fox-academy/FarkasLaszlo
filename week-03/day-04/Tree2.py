b5b07b9de8f06f384604ee358976fcbaf1cf7eae
FarkasLaszlo <wolfi1234@gmail.com>
2017-12-08 19:43:11 +0100

    some new files and changes

---
+++ week-03/day-04/Tree2.py
@@ -0,0 +1,30 @@
+import turtle
+
+def tree2(base,depth,angle):
+    if base > depth:
+        t.forward(base)
+        t.right(angle)
+        tree2(base-3,depth,angle)
+        t.left(2*angle)
+        tree2(base-3,depth,angle)
+        t.right(angle)
+        t.backward(base)
+
+
+
+
+
+t = turtle.Turtle()
+hex = turtle.Screen()
+t._tracer(0,0)
+t.color("yellow")
+hex.bgcolor("black")
+t.hideturtle()
+t.speed(0)
+t.penup()
+t.left(90)
+t.backward(250)
+t.pendown()
+tree2(50,5,13)
+hex.update()
+hex.exitonclick()
---
+++ week-03/day-04/hex solution.py
@@ -0,0 +1,52 @@
+import turtle
+
+def lot_hex(base,depth):
+    if base > depth:
+        t.speed(200)
+        for i in range(6):
+            t.forward(base)
+            t.right(60)
+        for i in range(6):
+            t.forward(base/2)
+            t.right(60)
+        lot_hex(base/2,depth-1)
+        t.forward(base)
+        t.right(60)
+        t.forward(base)
+        t.right(60)
+        lot_hex(base/2,depth-1)
+        for i in range(6):
+            t.forward(base/2)
+            t.right(60)
+        t.forward(base)
+        t.right(60)
+        t.forward(base)
+        t.right(60)
+        lot_hex(base/2,depth-1)
+        for i in range(6):
+            t.forward(base/2)
+            t.right(60)
+        t.forward(base)
+        t.right(60)
+        t.forward(base)
+        t.right(60)
+
+
+t = turtle.Turtle()
+hex = turtle.Screen()
+t._tracer(0,0)
+t.hideturtle()
+t.penup()
+t.left(90)
+t.forward(100)
+t.right(90)
+t.pendown()
+t.color("black")
+lot_hex(200,10)
+hex.update()
+hex.exitonclick()
+
+
+
+
+
