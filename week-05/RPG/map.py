437da58454141474fb53eb4b4eed8fe8aa276bcf
FarkasLaszlo <wolfi1234@gmail.com>
2017-12-19 15:17:15 +0100

    random monsters

--- week-05/RPG/map.py
+++ week-05/RPG/map.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import random
+from draw_random_map import *
 
 root = Tk()
 canvas = Canvas(root, width=720, height=792)
@@ -15,53 +15,52 @@ def read_file():
     return line
 
 
-class Character:
-    def __init__(self, hp, dp, sp):
+class Monsters:
+    def __init__(self, monster_type, canvas, max_hp=0, hp=0, dp=0, sp=0, ):
+        self.canvas = canvas
         self.hp = hp
         self.dp = dp
         self.sp = sp
-        self.d6 = random.randint(1, 6)
-
-
-class Monsters(Character):
-    def __init__(self, hp, dp, sp, monster_type):
-        super().__init__(hp, dp, sp)
+        self.max_hp = max_hp
         self.monster_type = monster_type
-        if monster_type == "Skeleton":
-            self.hp = 2 * self.d6
-            self.dp = 0.5 * self.d6
-            self.sp = self.d6
-        elif monster_type == "Boss":
-            self.hp = 3 * self.d6
-            self.dp = self.d6
-            self.sp = self.d6
         self.skeleton = PhotoImage(file="skeleton.png")
-        self.boss = PhotoImage(file="skeleton.png")
-
-    def draw_skeletons(self):
+        self.boss = PhotoImage(file="boss.png")
+        self.x = 0
+        self.y = 0
         line = read_file()
-        counter = 0
-        x = random.randint(72, 720)//72
-        y = random.randint(72, 792)//72
-        monster_list = []
-        while counter < 3:
-            if line[x][y] == "0":
-                pass
+        self.coord_list = []
+        i = 0
+        while i < 4:
+            self.x = random.randint(0, 720)//72
+            self.y = random.randint(0, 792)//72
+            if [self.x, self.y] not in self.coord_list and line[self.y][self.x] == "0" and self.x + self.y != 0:
+                self.coord_list.append([self.x, self.y])
+                i += 1
+
+    def draw_monsters(self):
+        for i in range(len(self.coord_list)):
+            x = self.coord_list[i][0]
+            y = self.coord_list[i][1]
+            if i < len(self.coord_list)-1:
+                self.canvas.create_image(x*72 + 36, y*72 + 36, image=self.skeleton)
+            else:
+                self.canvas.create_image(x * 72 + 36, y * 72 + 36, image=self.boss)
 
 
-class Hero(Character):
-    def __init__(self, canvas, hp=0, dp=0, sp=0):
-        super().__init__(hp, dp, sp)
+class Hero:
+    def __init__(self, canvas, max_hp=0, hp=0, dp=0, sp=0):
         self.canvas = canvas
+        self.hp = hp
+        self.max_hp = max_hp
+        self.dp = dp
+        self.sp = sp
         self.hero_down = PhotoImage(file="hero-down.png")
         self.hero_up = PhotoImage(file="hero-up.png")
         self.hero_left = PhotoImage(file="hero-left.png")
         self.hero_right = PhotoImage(file="hero-right.png")
         self.herox = 36
         self.heroy = 36
-        self.hp = 20 + 3 * self.d6
-        self.dp = 2 * self.d6
-        self.sp = self.d6
+        self.hp = 20
 
     def draw_down(self):
         self.canvas.create_image(self.herox, self.heroy, image=self.hero_down)
@@ -86,9 +85,12 @@ def draw_map(floor, wall):
                 canvas.create_image(x*72+36, 72*i+36, image=wall)
 
 
+draw_random_map()
 draw_map(floor, wall)
 hero1 = Hero(canvas)
 hero1.draw_right()
+monsters1 = Monsters("Skeleton", canvas)
+monsters1.draw_monsters()
 
 
 def on_key_press(e):
@@ -97,18 +99,22 @@ def on_key_press(e):
         draw_map(floor, wall)
         hero1.heroy = hero1.heroy - 72
         hero1.draw_up()
+        monsters1.draw_monsters()
     elif e.keycode == 40 and hero1.heroy < 756 and line[hero1.heroy//72 + 1][hero1.herox//72] == "0":
         draw_map(floor, wall)
         hero1.heroy = hero1.heroy + 72
         hero1.draw_down()
+        monsters1.draw_monsters()
     elif e.keycode == 37 and hero1.herox > 36 and line[hero1.heroy//72][hero1.herox//72 - 1] == "0":
         draw_map(floor, wall)
         hero1.herox = hero1.herox - 72
         hero1.draw_left()
+        monsters1.draw_monsters()
     elif e.keycode == 39 and hero1.herox < 684 and line[hero1.heroy//72][hero1.herox//72 + 1] == "0":
         draw_map(floor, wall)
         hero1.herox = hero1.herox + 72
         hero1.draw_right()
+        monsters1.draw_monsters()
 
 
 canvas.bind("<KeyPress>", on_key_press)
